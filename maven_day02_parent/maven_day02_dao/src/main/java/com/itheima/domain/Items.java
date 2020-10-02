5100ca44baf4bd45a966808b76a7ea1a9fd1a67b
zhangxin <az987961393@qq.com>
2020-10-02 10:51:07 +0800

    本地工程添加到git

---
+++ maven_day02_parent/maven_day02_dao/src/main/java/com/itheima/domain/Items.java
@@ -0,0 +1,63 @@
+package com.itheima.domain;
+
+import javax.xml.crypto.Data;
+
+public class Items {
+    private Integer id;
+    private String name;
+    private Double price;
+    private String pic;
+    private Data createtime;
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Data getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Data createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+
+
+}
